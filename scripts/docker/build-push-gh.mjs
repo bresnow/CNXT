17262a7cefe0b1914d8486bc983fa85601e14c45
βτεsηΘω <45741877+bresnow@users.noreply.github.com>
2022-06-14 22:27:33 +0000

    GHWF : import read from fsxx | 0.3.5
    Signed-off-by: GitHub <noreply@github.com>

--- scripts/docker/build-push-gh.mjs
+++ scripts/docker/build-push-gh.mjs
@@ -1,4 +1,6 @@
 import { $, chalk } from "zx";
+import { read } from "fsxx";
+
 let pkg = JSON.parse(await read('package.json'))
 let status = await $`git status`.pipe($`grep "On branch"`)
 let branch = status.stdout.replace("On branch ", "").trim()
