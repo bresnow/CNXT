4c395fa4f247e1afcf2b8d458ae31bab5e7905b3
βτεsηΘω <45741877+bresnow@users.noreply.github.com>
2022-06-15 01:01:02 +0000

    init
    Signed-off-by: GitHub <noreply@github.com>

--- scripts/docker/update-stack.mjs
+++ scripts/docker/update-stack.mjs
@@ -4,15 +4,7 @@ let pkg = JSON.parse(await read('package.json'))
 
 let image = pkg.name, version = pkg.version, stack
 let args = process.argv.slice(3);
-
-if (args.length > 0) {
-    for (let i = 0; i < args.length; i++) {
-        let arg = args[i]
-        if (arg.startsWith('--stack-name=' || '-s=' || '--stack-name' || '-s')) {
-            stack = arg.includes("=") ? arg.split('=')[1] : args[i + 1]
-        }
-    }
-}
-console.log(chalk.cyanBright(`Updating stack ${stack}`))
-await $`docker service update ${stack} --image=bresnow/${image}:${version}`
+let svc = await $`docker service ls --format  {{.Name}}`.pipe($`grep ${args[0]}`)
+console.log(chalk.cyanBright(`Updating stack ${args[0]}`))
+await $`docker service update ${svc} --image=bresnow/${image}:${version}`
 console.log(chalk.cyanBright('Fin'))
--- scripts/helpers/index.mjs
+++
@@ -1,7 +0,0 @@
-export async function getImageAndVersion() {
-    let pkg = JSON.parse(await read('package.json'))
-    let status = await $`git status`.pipe($`grep "On branch"`)
-    let branch = status.stdout.replace("On branch ", "").trim()
-    let image = pkg.name, version = pkg.version
-    return { image, version }
-}
