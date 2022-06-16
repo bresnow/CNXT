04c22da989421c5589c432622a2a4e7aff914377
βτεsηΘω <45741877+bresnow@users.noreply.github.com>
2022-06-16 08:12:59 +0000

    Default Push -  Jun 16 2022 08:12:58  | 0.3.58
    Signed-off-by: GitHub <noreply@github.com>

--- scripts/docker/swarm/service-log.mjs
+++ scripts/docker/swarm/service-log.mjs
@@ -3,6 +3,8 @@ import { read } from 'fsxx';
 let pkg = JSON.parse(await read('package.json'));
 import 'zx/globals';
 
-await $`docker service ls --format  {{.Name}}`.stdout.split('\n').forEach(async (line) => {
-  await $`docker service logs ${line}`;
-});
+await $`docker service ls --format  {{.Name}}`.stdout.toString()
+  .slice('\n')
+  .forEach(async (line) => {
+    await $`docker service logs ${line}`;
+  });
