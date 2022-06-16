0a9d7397a338c5261c71eb30a8a2f0d314f9e279
βτεsηΘω <45741877+bresnow@users.noreply.github.com>
2022-06-16 04:24:54 +0000

    "Update  Jun 16 2022 04:24:53  | 0.3.58
    Signed-off-by: GitHub <noreply@github.com>

--- scripts/git/push-all-modified.mjs
+++ scripts/git/push-all-modified.mjs
@@ -6,49 +6,50 @@ let message, version;
 let args = process.argv.slice(3);
 
 if (args.length > 0) {
-    for (let i = 0; i < args.length; i++) {
-        let arg = args[i];
+  for (let i = 0; i < args.length; i++) {
+    let arg = args[i];
 
-        if (arg.startsWith('--message=' || '-m=' || '--message' || '-m')) {
-            message = arg.includes('=') ? arg.split('=')[1] : args[i + 1];
-        }
-
-        if (arg.startsWith('--version=' || '-v=' || '--version' || '-v')) {
-            version = arg.includes('=') ? arg.split('=')[1] : args[i + 1];
-        }
-        if (arg.startsWith('--silent' || '-s')) {
-            $.verbose = false;
-        }
+    if (arg.startsWith('--message=' || '-m=' || '--message' || '-m')) {
+      message = arg.includes('=') ? arg.split('=')[1] : args[i + 1];
     }
+
+    if (arg.startsWith('--version=' || '-v=' || '--version' || '-v')) {
+      version = arg.includes('=') ? arg.split('=')[1] : args[i + 1];
+    }
+    if (arg.startsWith('--silent' || '-s')) {
+      $.verbose = false;
+    }
+  }
 }
 
 if (!message) {
-    let manswer = await question('Message for commit : ');
-    manswer !== ''
-        ? (message = manswer)
-        : (message = `"Update ${new Date(Date.now()).toString().slice(3, 25)}`);
+  let manswer = await question('Message for commit : ');
+  manswer !== ''
+    ? (message = manswer)
+    : (message = `"Update ${new Date(Date.now()).toString().slice(3, 25)}`);
 }
 
 if (!version) {
-    let vanswer = await question(
-        `Version ? \n ${chalk.bgCyan('Current Version ') + chalk.cyan(pkg.data.version)
-        }: `
-    );
-    if (vanswer !== '') {
-        version = vanswer;
-    } else {
-        version = pkg.data.version;
-        await pkg.save();
-    }
+  let vanswer = await question(
+    `Version ? \n ${
+      chalk.bgCyan('Current Version ') + chalk.cyan(pkg.data.version)
+    }: `
+  );
+  if (vanswer !== '') {
+    version = vanswer;
+  } else {
+    version = pkg.data.version;
+    await pkg.save();
+  }
 }
 
 let mod = await $`git status`.pipe($`grep modified:`);
-// Prettier writes all modified files 
+// Prettier writes all modified files
 mod.stdout.split('modified: ').forEach(async (line) => {
-    let filename = line.trim();
-    if (filename.length > 1) {
-        await $`npx prettier --write ${filename}`;
-    }
+  let filename = line.trim();
+  if (filename.length > 1) {
+    await $`npx prettier --write ${filename}`;
+  }
 });
 await $`git add --all`;
 await $`git commit -s -m ${`${message} | ${version}`}`;
