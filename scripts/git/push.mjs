35855fc2a679c818a1858b5257c446ea538d209c
βτεsηΘω <45741877+bresnow@users.noreply.github.com>
2022-06-16 08:57:48 +0000

    Push Script | 0.3.6
    Signed-off-by: GitHub <noreply@github.com>

--- scripts/git/push.mjs
+++ scripts/git/push.mjs
@@ -1,58 +1,52 @@
-import { $, question, YAML, chalk, sleep } from 'zx';
-import { io } from 'fsxx';
+import { $, question, YAML, chalk, sleep } from 'zx'
+import { io } from 'fsxx'
 import 'zx/globals';
-let pkg = await io.json`package.json`;
-let message, version;
-let args = process.argv.slice(3);
+let pkg = await io.json`package.json`
+let message, version
+let args = process.argv.slice(3)
 
 if (args.length > 0) {
+
   for (let i = 0; i < args.length; i++) {
-    let arg = args[i];
+
+    let arg = args[i]
 
     if (arg.startsWith('--message=' || '-m=' || '--message' || '-m')) {
-      message = arg.includes('=') ? arg.split('=')[1] : args[i + 1];
+
+      message = arg.includes("=") ? arg.split('=')[1] : args[i + 1]
     }
 
     if (arg.startsWith('--version=' || '-v=' || '--version' || '-v')) {
-      version = arg.includes('=') ? arg.split('=')[1] : args[i + 1];
+
+      version = arg.includes("=") ? arg.split('=')[1] : args[i + 1]
     }
+    if (arg.startsWith('--silent' || '-s')) {
+
+      $.verbose = false
+
+    }
+
   }
+
 }
-$.verbose = false;
-if (!message) {
-  let manswer = await question(chalk.green('\n Message for commit '));
-  if (manswer !== '') {
-    message = manswer;
-  } else {
-    message = `Default Push - ${new Date(Date.now()).toString().slice(3, 25)}`;
-    console.log(chalk.yellow(`Using default message`));
-    console.log(chalk.blueBright(`${message}`));
-  }
-}
-if (!version) {
-  let vanswer = await question(
-    `${'New Version?'} ${
-      chalk.cyan('Current Version ') + chalk.cyan(pkg.data.version)
-    }: `
-  );
-  if (vanswer !== '') {
-    version = vanswer;
-    version = pkg.version;
-    await pkg.save();
-  } else {
-    version = pkg.version;
-    await pkg.save();
+
+if (message === undefined) {
+  message = await question("Message for commit : ")
+  if (message === '' || message === ' ' || typeof message === 'undefined') {
+    message = 'Update'
   }
 }
 
-let mod = await $`git status`.pipe($`grep modified:`);
-// Prettier writes all modified files
-mod.stdout.split('modified: ').forEach(async (line) => {
-  let filename = line.trim();
-  if (filename.length > 1) {
-    await $`npx prettier --write ${filename}`;
-  }
-});
-await $`git add --all`;
-await $`git commit -s -m ${`${message} | ${version}`}`;
-await $`git push -uf ${await $`git remote show`} ${await $`git branch --show-current`}`;
+if (version === undefined) {
+  version = await question(`Version ? \n ${chalk.bgCyan('Current Version ') + chalk.cyan(pkg.data.version)}: `)
+  typeof version === 'undefined' ? version = (Number(pkg.data.version) + .01).toString().trim() : version = version.trim()
+}
+
+//PACKAGE>JSON MODIFY VERSION
+
+pkg.data.version = version
+await pkg.save()
+await $`git status`
+await $`git add --all`
+await $`git commit -s -m ${`${message} | ${version}`}`
+await $`git push -uf ${await $`git remote show`} ${await $`git branch --show-current`}`
