ca32827301eee5ce2cecc44f317cbc775e998d3d
βτεsηΘω <45741877+bresnow@users.noreply.github.com>
2022-06-16 05:25:47 +0000

    Default Push -  Jun 16 2022 05:25:47  | 0.3.58
    Signed-off-by: GitHub <noreply@github.com>

--- scripts/docker/swarm/service-log.mjs
+++ scripts/docker/swarm/service-log.mjs
@@ -2,12 +2,12 @@ import { $, chalk, question } from 'zx';
 import { read } from 'fsxx';
 let pkg = JSON.parse(await read('package.json'));
 
-let service = pkg.name
+let service = pkg.name;
 let svc = await $`docker service ls --format  {{.Name}}`.pipe(
-    $`grep ${service}`
+  $`grep ${service}`
 );
 
 let arr = svc.split('\n');
-arr.forEach(async element => {
-    await $`docker service logs ${element}`;
+arr.forEach(async (element) => {
+  await $`docker service logs ${element}`;
 });
