b213f1591acebf0da46e4252f5b601c1fd68154b
βτεsηΘω <45741877+bresnow@users.noreply.github.com>
2022-06-16 22:21:56 +0000

    PKG UTILS | 0.3.65
    Signed-off-by: GitHub <noreply@github.com>

--- scripts/pkg-utils.ts
+++ scripts/pkg-utils.ts
@@ -4,10 +4,14 @@ import { read } from 'fsxx';
 async function getConfig() {
   let args = process.argv.slice(3);
   let pkg = JSON.parse(await read('package.json'));
-  console.log({
-    name: `${pkg.name.replace('.', '-')}`,
-    version: `${pkg.version}`,
-  });
+  for (let i = 0; i < args.length; i++) {
+    let arg = args[i];
+    if (arg.startsWith('--name ' || '-n')) {
+      console.log(`${pkg.name.replace('.', '-')}`);
+    }
+    if (arg.startsWith('--version ' || '-v')) {
+      console.log(`${pkg.version}`);
+    }
+  }
 }
-
 getConfig();
