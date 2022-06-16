3004d92280f69eb1161a1dfb0820ca1c453e82b6
βτεsηΘω <45741877+bresnow@users.noreply.github.com>
2022-06-16 17:27:26 +0000

    GH Actions | 0.3.61
    Signed-off-by: GitHub <noreply@github.com>

--- scripts/docker/swarm/stack-utils.mjs
+++ scripts/docker/swarm/stack-utils.mjs
@@ -1,13 +1,8 @@
-import { $ } from 'zx';
 import 'zx/globals';
 import {
     YAML
 } from 'zx'
 import { read } from 'fsxx';
-
+let args = process.argv.slice(3)
 let serviceNames = Object.keys(YAML.parse(await read('swarm-stacks/remix-gun.yml')).services).map(x => x.split('.')[0])
-
-serviceNames.forEach(serviceName => {
-    await $`docker service logs ${pkg.name.replace('.', '-')}_${serviceName}`;
-})
-
+console.log(serviceNames)
