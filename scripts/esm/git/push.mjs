cac32fd711c3290f988fcb5694995e743fb2b282
βτεsηΘω <45741877+bresnow@users.noreply.github.com>
2022-06-17 18:01:47 +0000

    Default Commit 6/17/2022, 6:01:44 PM | 0.3.69 | 0.3.69
    Signed-off-by: GitHub <noreply@github.com>

--- scripts/esm/git/push.mjs
+++ scripts/esm/git/push.mjs
@@ -54,4 +54,4 @@ try {
 }
 await $`git add --all`;
 await $`git commit -s -m ${`${message} | ${version}`}`;
-await $`git push -uf ${await $`git remote show`} ${await $`git branch --show-current`}`;
+await $`git push -f ${await $`git remote show`} ${await $`git branch --show-current`}`;
