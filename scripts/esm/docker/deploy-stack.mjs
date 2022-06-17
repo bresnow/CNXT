0d76c9a1f508227403829aa6edfaa09bf2f7da5e
βτεsηΘω <45741877+bresnow@users.noreply.github.com>
2022-06-17 04:41:57 +0000

    Static Logs | 0.3.683
    Signed-off-by: GitHub <noreply@github.com>

--- scripts/esm/docker/deploy-stack.mjs
+++ scripts/esm/docker/deploy-stack.mjs
@@ -4,7 +4,7 @@ import { read } from 'fsxx';
 
 async function deploy() {
   let pkg = JSON.parse(await read('package.json'));
-  let name = pkg.name.replace('.', '-');
+  let name = pkg.name;
   let version = pkg.version;
   console.log(chalk.cyanBright(`Deploying Stack ${name}`));
   await $`docker stack deploy -c swarm-stacks/traefik.yml ${name}`;
--- scripts/esm/docker/swarm/service-log.mjs
+++ scripts/esm/docker/swarm/service-log.mjs
@@ -2,14 +2,19 @@ import { $ } from 'zx';
 import 'zx/globals';
 import { YAML } from 'zx';
 import { read } from 'fsxx';
+let cl = console.log;
 async function logs() {
   let pkg = JSON.parse(await read('package.json'));
-  let serviceNames = Object.keys(
+  let services = Object.keys(
     YAML.parse(await read('swarm-stacks/remix-gun.yml'))
   );
-  serviceNames.forEach(async (serviceName) => {
-    await $`docker service logs ${pkg.name.replace('.', '-')}_${serviceName}`;
-  });
+
+  let str = YAML.stringify(await read('swarm-stacks/remix-gun.yml'));
+
+  cl(JSON.parse(JSON.stringify(str)));
+  // services.forEach(async (serviceName) => {
+  //   await $`docker service logs ${pkg.name.replace('.', '-')}_${serviceName}`;
+  // });
 }
 
 await logs();
