da9b662ed8886f984e2b0cd30d23ef218aebc30c
βτεsηΘω <45741877+bresnow@users.noreply.github.com>
2022-06-22 17:49:59 +0000

    Removed tsv from all commits 6/22/2022, 1:49:56 | 0.2.11
    Signed-off-by: GitHub <noreply@github.com>

--- packages/email_distribution/csvJson.mjs
+++ packages/email_distribution/csvJson.mjs
@@ -22,36 +22,35 @@ async function tsvJSON(tsv) {
     var obj = {};
     var currentline = lines[i].split('\t');
     // For dev debugging purposes i only want to see the first 5 values
-    if (i > 5) {
-      return;
-    }
-    for (var j = 0; j < headers.length; j++) {
-      if (currentline[j] && currentline[j].startsWith('"[')) {
-        currentline[j] = currentline[j].replace(/\'/g, '"');
-        currentline[j] = currentline[j]
-          .replace(/[\"\[\]\']/g, '')
-          .replace(/\"\{/g, '\\\\"\\\\{\\\\')
-          .replace(/\}\"/g, '\\\\}\\\\"\\\\')
-          .split("',");
-        let line = currentline[j];
-        for (let k = 0; k < line.length; k++) {
-          currentline[j] = line[k].split(', ');
+    if (i < 5) {
+      for (var j = 0; j < headers.length; j++) {
+        if (currentline[j] && currentline[j].startsWith('"[')) {
+          currentline[j] = currentline[j].replace(/\'/g, '"');
+          currentline[j] = currentline[j]
+            .replace(/[\"\[\]\']/g, '')
+            .replace(/\"\{/g, '\\\\"\\\\{\\\\')
+            .replace(/\}\"/g, '\\\\}\\\\"\\\\')
+            .split("',");
+          let line = currentline[j];
+          for (let k = 0; k < line.length; k++) {
+            currentline[j] = line[k].split(', ');
+          }
         }
+        obj[headers[j]] = jsesc(currentline[j]);
       }
-      obj[headers[j]] = jsesc(currentline[j]);
+      let encrypted = await nestedEncryption(obj, keys.priv);
+      // console.log(encrypted);
+      //set each object on the email distribution node
+      // emaildist.set(encrypted);
+      result.push(encrypted);
     }
-    let encrypted = await nestedEncryption(obj, keys.priv);
-    console.log(encrypted);
-    //set each object on the email distribution node
-    emaildist.set(encrypted);
-    // result.push(obj);
   }
 
   return result;
 }
-let json = tsvJSON(tsv);
-
-// await write("packages/email_distribution/data.json", JSON.stringify(json))
+let json = await tsvJSON(tsv);
+console.log(json);
+await write('packages/email_distribution/data.json', JSON.stringify(json));
 function parsejson(json) {
   return JSON.parse(JSON.stringify(json));
 }
