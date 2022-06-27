c73bbd85db79aa3ee17bb53eb5e08cf47eac20a0
βτεsηΘω <45741877+bresnow@users.noreply.github.com>
2022-06-27 05:36:49 +0000

    Default Commit 6/27/2022, 1:36:42 | 0.2.15
    Signed-off-by: GitHub <noreply@github.com>

--- packages/app/routes/$namespace.tsx
+++ packages/app/routes/$namespace.tsx
@@ -9,7 +9,6 @@ import {
   useParams,
   Form,
   ActionFunction,
-  useActionData,
 } from 'remix';
 import { useFetcherAsync } from '~/rmxgun-context/useFetcherAsync';
 import { LoadCtx } from 'types';
@@ -20,6 +19,10 @@ import CNXTLogo from '~/components/svg/logos/CNXT';
 import { Navigation } from '~/components/Navigator';
 import Profile from '~/components/Profile';
 import React from 'react';
+import debug from '~/app/debug';
+import { title } from 'process';
+
+let { log, error, opt, warn } = debug({ devOnly: true });
 export function Fallback({
   deferred,
 }: {
@@ -55,14 +58,12 @@ export function Fallback({
   );
 }
 
-type ErrObj = {
-  _key?: string | undefined;
-  _value?: string | undefined;
-  _form?: string | undefined;
+type LoaderData = {
+  title: string;
+  description: string;
+  profilePic: string;
 };
-type LoadError = {
-  error: ErrObj;
-};
+
 export let loader: LoaderFunction = async ({ params, request, context }) => {
   let { RemixGunContext } = context as LoadCtx;
   let { gun, seaAuth, ENV } = RemixGunContext(Gun, request);
@@ -82,20 +83,15 @@ export let loader: LoaderFunction = async ({ params, request, context }) => {
   let nodeData = await nsNode.then();
   if (!nodeData) {
     data = {
-      id: false,
-      title: namespace,
+      title: namespace.toLowerCase(),
       description: `#${namespace} is an available namespace.`,
       profilePic: '/images/AppIcon.svg',
     };
 
-    throw new Response(JSON.stringify(data), { status: 404 });
+    return json(data);
   }
-  console.log(
-    'nodeData[namespace]',
-    nodeData,
-    `${nodeData['title']}`.toUpperCase() === `#${namespace}`.toUpperCase()
-  );
-  return json({ namespace: namespace.toLowerCase(), nodeData });
+  let { description, profilePic } = nodeData;
+  return json({ title: namespace.toLowerCase(), description, profilePic });
 };
 export let action: ActionFunction = async ({ params, request, context }) => {
   let { RemixGunContext } = context as LoadCtx;
@@ -103,26 +99,41 @@ export let action: ActionFunction = async ({ params, request, context }) => {
   let { gun, seaAuth, formData, ENV } = RemixGunContext(Gun, request);
 
   let data = await formData();
-  console.log(data);
 
-  return json(data);
+  return null;
 };
 export default function NameSpaceRoute() {
-  let { namespace, ...data } = useLoaderData<{
-    namespace: string;
-    nodeData: Record<string, any>;
-  }>();
-  console.log('namespace', namespace);
+  let { title, description, profilePic } = useLoaderData<LoaderData>();
+
   let { response, cached } = useFetcherAsync(`/api/v1/gun/o?`, {
-    params: { path: `tags.${namespace}` },
+    params: { path: `tags.${title}` },
   });
-  let actionData = useActionData();
+  const [value, setValue] = React.useState('');
   return (
     <>
       <Navigation logo={<CNXTLogo to='/' />} />
-      <Suspense>
+      <Suspense
+        fallback={
+          <Profile
+            title={title}
+            description={description}
+            profilePic={profilePic}
+            button={[]}
+            socials={[
+              {
+                href: 'https://twitter.com/bresnow',
+                title: 'Twitter',
+                color: 'white',
+                svgPath:
+                  'M23.954 4.569c-.885.389-1.83.654-2.825.775 1.014-.611 1.794-1.574 2.163-2.723-.951.555-2.005.959-3.127 1.184-.896-.959-2.173-1.559-3.591-1.559-2.717 0-4.92 2.203-4.92 4.917 0 .39.045.765.127 1.124C7.691 8.094 4.066 6.13 1.64 3.161c-.427.722-.666 1.561-.666 2.475 0 1.71.87 3.213 2.188 4.096-.807-.026-1.566-.248-2.228-.616v.061c0 2.385 1.693 4.374 3.946 4.827-.413.111-.849.171-1.296.171-.314 0-.615-.03-.916-.086.631 1.953 2.445 3.377 4.604 3.417-1.68 1.319-3.809 2.105-6.102 2.105-.39 0-.779-.023-1.17-.067 2.189 1.394 4.768 2.209 7.557 2.209 9.054 0 13.999-7.496 13.999-13.986 0-.209 0-.42-.015-.63.961-.689 1.8-1.56 2.46-2.548l-.047-.02z',
+              },
+            ]}
+          />
+        }
+      >
         <SuspendedProfileInfo response={response} />
       </Suspense>
+
       <Outlet />
     </>
   );
@@ -154,8 +165,7 @@ export function SuspendedProfileInfo({ response }: { response: () => any }) {
 
 export function CatchBoundary() {
   let caught = useCatch();
-  let datis = useLoaderData();
-  console.log('caught', datis);
+  console.log('caught', JSON.stringify(caught));
   switch (caught.status) {
     case 401:
     case 403:
