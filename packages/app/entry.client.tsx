143c2276b9b41ed3b1b98f6a7078d9f51a911fd0
βτεsηΘω <45741877+bresnow@users.noreply.github.com>
2022-06-30 01:03:04 +0000

    Default Commit 6/29/2022, 9:02:59 | 0.2.17
    Signed-off-by: GitHub <noreply@github.com>

--- packages/app/entry.client.tsx
+++ packages/app/entry.client.tsx
@@ -7,6 +7,17 @@ ReactDOM.hydrateRoot(document, <RmxGun.Client />);
 
 if ('serviceWorker' in navigator) {
   // Use the window load event to keep the page load performant
+  const Gun = window.Gun,
+    gun = new Gun({ localStorage: false }),
+    user = gun.user();
+  // I just like using window with gun... makes me feel safe okay??
+  let { routeData } = __remixContext,
+    { root, gunOpts } = routeData.root,
+    AppKeys = root.ENV.APP_KEY_PAIR;
+  let peered = new Gun(gunOpts);
+  gun.on('auth', (msg) => {
+    console.log(`c%${msg}`, 'color: green; font-weight: bold;', msg);
+  });
   window.addEventListener('load', () => {
     navigator.serviceWorker
       .register('/entry.worker.js', { type: 'module' })
